0cef025e3726b6ce6e421ca66ca691637b16ac86
dbvieyy <dbivyy@gmail.com>
2013-09-12 22:36:00 +1000

    Create Email_Autherticator.java

---
+++ Projects/Doodle/CSE-Doodle/src/Standard/Email_Autherticator.java
@@ -0,0 +1,21 @@
+package standard;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+public class Email_Autherticator extends Authenticator {
+	String username = "mailscomp";
+	
+	String password = "!@#123qwe";
+	public Email_Autherticator() {
+		super();
+	}
+	public Email_Autherticator(String user,String pwd){
+		super();
+		username = user;
+		password = pwd;
+	}
+	public PasswordAuthentication getPasswordAuthentication(){
+		return new PasswordAuthentication(username,password);
+	}
+}
