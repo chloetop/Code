9b9275bf714b88d8f73770cea268d46693aabf4a
dbvieyy <dbivyy@gmail.com>
2013-09-12 22:36:43 +1000

    Create InfoObj.java

---
+++ Projects/Doodle/CSE-Doodle/src/Standard/InfoObj.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package standard;
+
+/**
+ *
+ * @author Administrator
+ */
+public class InfoObj {
+// create table Poll(
+//Poll_ID varchar(100) primary key,
+//Poll_Title varchar(100),
+//Poll_Description varchar(100),
+//Poll_Initiator varchar(100),
+//Poll_Type int,
+//Poll_Lastmodify varchar(100),
+//Poll_Descsion varchar(100),
+//Poll_Status int
+//);
+private String P_ID = "";
+private String P_Title = "";
+private String P_Description = "";
+private String P_Initiator = "";
+private String P_Type = "";
+private String P_Lastmodify = "";
+private String P_Descsion = "";
+private String P_Status = "";
+
+private String O_Id = "";
+private String O_Content = "";
+private String P_SubCon = "";
+//Option_ID varchar(100) primary key,
+//Option_Content varchar(100),
+//Option_SubCon varchar(100)
+private String C_ID = "";
+private String C_Creator = "";
+private String C_Content = "";
+private String C_DateTime = "";
+//Comment_ID varchar(100) primary key,
+//Comment_Poll varchar(100),
+//Comment_Creator varchar(100),
+//Comment_Content varchar(100),
+//Comment_DateTime varchar(100)
+//);
+//
+//create table Vote(
+//Vote_ID varchar(100) primary key,
+//Vote_Creator varchar(100),
+//Vote_Option varchar(100),
+//Vote_Poll varchar(100)
+//);   
+}
