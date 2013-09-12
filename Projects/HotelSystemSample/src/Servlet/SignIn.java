8f697eeb1ce7bf24622abca9ded85f5b97c645e9
dbvieyy <dbivyy@gmail.com>
2013-09-12 23:32:07 +1000

    Create SignIn.java

---
+++ Projects/HotelSystemSample/src/Servlet/SignIn.java
@@ -0,0 +1,169 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MyServlet;
+
+import PublicClass.DataBase;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.apache.catalina.Session;
+
+/**
+ *
+ * @author Administrator
+ */
+public class SignIn extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /*
+             * TODO output your page here. You may use following sample code.
+             */
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet SignIn</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet SignIn at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+
+
+            String C_ID = request.getParameter("account").trim();
+            String PASSWORD = request.getParameter("pwd").trim();
+
+            DataBase db = new DataBase();
+            String sql = "Select Password,Role,RIGHTS From Users where ID='" + C_ID + "'";
+            ResultSet rs = db.ExecuteSQL(sql);
+            String strComp = "";
+            String rights = "";
+            int Role = 0;
+
+            while (rs.next()) {
+                strComp = rs.getString("Password");
+                Role = Integer.parseInt(rs.getString("Role"));
+                rights = rs.getString("RIGHTS");
+            }
+
+            if (rs.getRow() > 0) {
+                if (PASSWORD.equals(strComp)) {
+                    // out.println("ok");
+                    HttpSession session = request.getSession();
+                    session.setAttribute("UID", strComp);
+                    if (Role == 2) {
+                        session.setAttribute("RIGHTS", rights);
+                    }
+                    if (Role == 1) {
+                        response.sendRedirect("index.jsp");
+                    }
+                    if (Role == 2) {
+                        response.sendRedirect("Manager/m_manage.jsp");
+                    }
+                    if (Role == 3) {
+                        response.sendRedirect("Owner/o_manage.jsp");
+                    }
+
+                } else {
+                    //out.println(" not ok");
+                    if (Role == 1) {
+                        response.sendRedirect("error.jsp?des=Password is not correct!&page=consumer.jsp");
+                    }
+                    if (Role == 2) {
+
+                        response.sendRedirect("error.jsp?des=Password is not correct!&page=manager_index.jsp");
+                    }
+                    if (Role == 3) {
+
+                        response.sendRedirect("error.jsp?des=Password is not correct!&page=owner_index.jsp");
+                    }
+
+                }
+            } else {
+                // out.println(" no found");
+
+                if (Role == 1) {
+                    response.sendRedirect("error.jsp?des=not a registed user!&page=consumer.jsp");
+                }
+                if (Role == 2) {
+
+                    response.sendRedirect("error.jsp?des=not a registed user!&page=manager_index.jsp");
+                }
+                if (Role == 3) {
+
+                    response.sendRedirect("error.jsp?des=not a registed user!&page=owner_index.jsp");
+                }
+
+            }
+        } catch (Exception ex) {
+            out.println(" error");
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
